a325c38a77d93b534991062164902886f452f44d
kde0820 <kde0820@naver.com>
2019-07-10 15:47:35 +0900

    Beautify code

--- data_structure/linked_list/TestLinkedList.java
+++ data_structure/linked_list/TestLinkedList.java
@@ -1,15 +1,15 @@
 import java.util.Random;
 
 public class TestLinkedList {
-	
+
 	public static void main(String[] args) {
-		Random number = new Random();		
+		Random number = new Random();
 		LinkedList LinkedList1 = new LinkedList();
 		LinkedList LinkedList2 = new LinkedList();
-		
+
 		int i;
 		System.out.println("***** Insert LinkedList *****");
-		for(i = 0; i < 10; i++){
+		for (i = 0; i < 10; i++) {
 			int num = number.nextInt(100);
 			LinkedList1.insert(num);
 		}
@@ -18,17 +18,16 @@ public class TestLinkedList {
 		System.out.println("***** Reverse LinkedList *****");
 		LinkedList1.reverse();
 		LinkedList1.print();
-		
+
 		System.out.println("***** Insert LinkedList *****");
-		for(i = 0; i < 10; i++){
+		for (i = 0; i < 10; i++) {
 			int num = number.nextInt(100);
 			LinkedList2.insert(num);
 		}
 		LinkedList2.print();
-		
+
 		LinkedList1.merge(LinkedList2);
-		System.out.println("***** Merge LinkedList *****");		
+		System.out.println("***** Merge LinkedList *****");
 		LinkedList1.print();
 	}
-
 }
